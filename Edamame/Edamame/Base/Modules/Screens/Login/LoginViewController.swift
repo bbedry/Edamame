2b2be219ab3e6ba96b6838d35db989a5a818d0fc
master <beddoan@gmail.com>
2024-01-25 23:30:59 +0300

    loginViewController ui completed.

--- Edamame/Edamame/Base/Modules/Screens/Login/LoginViewController.swift
+++ Edamame/Edamame/Base/Modules/Screens/Login/LoginViewController.swift
@@ -22,21 +22,75 @@ protocol LoginViewProtocol: AnyObject {
 
 class LoginViewController: UIViewController, LoginViewProtocol {
 
+    @IBOutlet weak var emailContainerView: UIView!
+    @IBOutlet weak var emailTextField: UITextField!
+    @IBOutlet weak var passwordContainerView: UIView!
+    @IBOutlet weak var passwordTextField: UITextField!
+    @IBOutlet weak var forgotPasswordLabel: UILabel!
+    @IBOutlet weak var loginButton: UIButton!
+    @IBOutlet weak var googleLoginButton: UIButton!
+    @IBOutlet weak var showPasswordButton: UIButton!
     
     
     var handler: LoginViewHandler?
+    var isShowPass: Bool = false
     
     override func viewDidLoad() {
         super.viewDidLoad()
 
+        setupUI()
     }
     
-
+    @IBAction func emailTextField(_ sender: UITextField) {
+    }
+    
+    @IBAction func passwordTextField(_ sender: UITextField) {
+        
+    }
+    
+    @IBAction func loginButtonTapped(_ sender: UIButton) {
+        
+    }
+    @IBAction func loginWithGoogleTapped(_ sender: UIButton) {
+        
+    }
+    
+    @IBAction func signUpButtonTapped(_ sender: UIButton) {
+        
+    }
+    @IBAction func showablePassword(_ sender: UIButton) {
+        if isShowPass != false  {
+            passwordTextField.isSecureTextEntry = true
+            self.isShowPass = false
+        } else {
+            passwordTextField.isSecureTextEntry = false
+            self.isShowPass = true
+        }
+    }
+    
     func showEmailError(_ error: String) {
         print(error)
     }
     
     func showPasswordError(_ error: String) {
         print(error)
+    }
+    
+    
+    
+}
+
+//MARK: Setup loginViewController Components UI
+extension LoginViewController {
+    func setupUI() {
+        emailContainerView.addBorderView(width: 0.5, color: AppColors.outlineColor)
+        emailContainerView.addCornerRadius(radius: 25)
+        
+        passwordContainerView.addBorderView(width: 0.5, color: AppColors.outlineColor)
+        passwordContainerView.addCornerRadius(radius: 25)
+        
+        loginButton.addCornerRadius(radius: 25)
+        googleLoginButton.addCornerRadius(radius: 25)
+        
     }
 }
---
+++ Edamame/Edamame/Base/Utilies/AppColors.swift
@@ -0,0 +1,18 @@
+//
+//  AppColors.swift
+//  Edamame
+//
+//  Created by Bedri Doğan on 25.01.2024.
+//
+
+import UIKit
+
+
+class AppColors {
+    static var primaryColor =  UIColor.init(red: 31.0/255.0, green: 204.0/255.0, blue: 121.0/255.0, alpha: 1.0)
+    static var secondryColor = UIColor.init(red: 255.0/255.0, green: 100.0/255.0, blue: 100.0/255.0, alpha: 1.0)
+    static var mainTextColor = UIColor.init(red: 46.0/255.0, green: 62.0/255.0, blue: 92.0/255.0, alpha: 1.0)
+    static var secondryTextColor = UIColor.init(red: 159.0/255.0, green: 165.0/255.0, blue: 192.0/255.0, alpha: 1.0)
+    static var outlineColor = UIColor.init(red: 208.0/255.0, green: 219.0/255.0, blue: 234.0/255.0, alpha: 1.0)
+    static var formTextColor = UIColor.init(red: 244.0/255.0, green: 245.0/255.0, blue: 247.0/255.0, alpha: 1.0)
+}
